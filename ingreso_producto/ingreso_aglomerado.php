e248b7e34521ec303c33026bd2706affcd740bb5
pipiyo <fe.cavieres@gmail.com>
2016-08-03 16:05:50 -0400

    volcar categorias producto

--- ingreso_producto/ingreso_aglomerado.php
+++ ingreso_producto/ingreso_aglomerado.php
@@ -1,11 +1,13 @@
-<?php require_once('../Conexion/Conexion.php'); 
+<?php require_once('../Conexion/Query_ingreso_aglomerado.php'); 
+
+	$con = new Conexion\Query_ingreso_aglomerado;
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 
   <head>
-  <title> Informe OT final V1.0</title>
+  <title>Ingreso Aglomerado</title>
   <meta name="description" content="free website template" />
   <meta name="keywords" content="enter your keywords here" />
   <meta http-equiv="content-type" content="text/html; charset=utf-8" />
@@ -16,15 +18,42 @@
 
   <script language = javascript>
 
+  	
+
 
 
 
   </script>  
+  
 </head>
 <body>	
  <div class="container">
  	<div class="row">
- 		<input type="" name="">
+ 		
+ 		<select class="textbox" name="categoria" id="categoria"  >
+    		<option></option>
+		 	<?php
+
+		 		foreach ($n = $con->Query('SELECT * FROM categoria_producto') as $key => $value) {
+		 			echo "<option>" . $n[$key]['nombre'] . "</option>";
+		 		};
+
+		 	?>
+  		</select>
+
+  		<input type="text" name="codigo" />
+  		<input type="text" name="descripcion" />
+
+ 	</div>
+
+
+
+ 	<div class="row">
+ 		<pre>
+		 	<?php
+		 		var_dump($con->Query('SELECT * FROM categoria_producto'));
+		 	?>
+ 		</pre>
  	</div>
  	
  </div>
