3197ecd053d1462d937d69031f65f7c4eeef7c13
Cristóbal Maturana <cri.maturana@gmail.com>
2017-03-27 11:26:36 -0300

    ocpdf

--- pdfOC.php
+++ pdfOC.php
@@ -1,12 +1,5 @@
 <?php
 require_once('Conexion/Conexion.php');
-session_start();
-if (!$_SESSION){
-echo '<script language = javascript>
-alert("usuario no autenticado")
-self.location = "index.php"
-</script>';
-}
 require_once('script-descripcion-oc.php');
 include('convertToPDF.php');
 
